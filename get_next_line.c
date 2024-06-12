1762238e0db761e1b8084e52f1c41c33de596006
Gabriel Levin <glevin@c3a1c7.42berlin.de>
2024-06-12 20:09:51 +0200

    refactor: breaking the code into smaller components

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: glevin <glevin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 11:29:50 by glevin            #+#    #+#             */
-/*   Updated: 2024/06/11 18:01:16 by glevin           ###   ########.fr       */
+/*   Updated: 2024/06/12 20:08:17 by glevin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,16 +42,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
-// char	*ft_strdup(const char *string, const int n)
-// {
-// 	char	*str2;
-
-// 	str2 = malloc(n + 1 * sizeof(char));
-// 	if (str2 == NULL)
-// 		return (NULL);
-// 	return ((char *)ft_memcpy(str2, string, n));
-// }
 
 char	*ft_strdup(const char *string)
 {
@@ -122,18 +112,10 @@ int	ft_strchr(const char *str, int c)
 	return (i);
 }
 
-char	*get_next_line(int fd)
+char	*add_to_stash(char *stash, char *buf)
 {
-	char		*buf;
-	char		*next_line;
-	char		*tmp_stash;
-	static char	*stash;
-	int			eol_loc;
-	int			byte_cnt;
+	char	*tmp_stash;
 
-	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (buf == NULL)
-		return (NULL);
 	if (stash == NULL)
 	{
 		stash = (char *)malloc(1);
@@ -144,41 +126,65 @@ char	*get_next_line(int fd)
 		}
 		stash[0] = '\0';
 	}
+	tmp_stash = ft_strjoin(stash, buf);
+	if (tmp_stash == NULL)
+	{
+		free(stash);
+		return (NULL);
+	}
+	free(stash);
+	return (tmp_stash);
+}
+
+char	*check_new_line(char **stash)
+{
+	int		eol_loc;
+	char	*next_line;
+	char	*tmp_stash;
+
+	eol_loc = ft_strchr(*stash, '\n');
+	if (eol_loc != 0)
+	{
+		next_line = ft_substr(*stash, 0, eol_loc + 1);
+		tmp_stash = ft_strdup(*stash + eol_loc + 1);
+		if (tmp_stash == NULL)
+		{
+			free(*stash);
+			return (NULL);
+		}
+		free(*stash);
+		*stash = tmp_stash;
+		return (next_line);
+	}
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buf;
+	char		*next_line;
+	static char	*stash;
+	int			byte_cnt;
+
+	// Handle errors with fd
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (buf == NULL)
+		return (NULL);
 	while ((byte_cnt = read(fd, buf, BUFFER_SIZE)) != 0)
 	{
 		buf[byte_cnt] = '\0';
-		// Get the next n bytes and add them to stash
-		tmp_stash = ft_strjoin(stash, buf);
-		if (tmp_stash == NULL)
-		{
-			free(stash);
-			free(buf);
-			return (NULL);
-		}
-		free(stash);
-		stash = tmp_stash;
-		// If stash has a new line
-		eol_loc = ft_strchr(stash, '\n');
-		if (eol_loc != 0)
-		{
-			next_line = ft_substr(stash, 0, eol_loc);
-			tmp_stash = ft_strdup(stash + eol_loc + 1);
-			if (tmp_stash == NULL)
-			{
-				free(stash);
-				free(buf);
-				return (NULL);
-			}
-			free(stash);
-			stash = tmp_stash;
-			free(buf);
-			// printf("stash: %s\n", stash);
-			return (next_line);
-		}
+		stash = add_to_stash(stash, buf);
+		next_line = check_new_line(&stash);
+		if (next_line)
+			return next_line;
 	}
 	if (byte_cnt < 0)
 	{
 		free(buf);
+		free(stash);
+		stash = NULL;
 		return (NULL);
 	}
 	// Handle end of file
@@ -196,22 +202,22 @@ char	*get_next_line(int fd)
 	return (NULL);
 }
 
-// int	main(void)
-// {
-// 	int fd;
-// 	int i;
-// 	char *line;
+int	main(void)
+{
+	int fd;
+	int i;
+	char *line;
 
-// 	i = 0;
+	i = 0;
 
-// 	fd = open("test2.txt", O_RDONLY);
-// 	while ((line = get_next_line(fd)) != NULL && i < 20)
-// 	{
-// 		// printf("fd: %i", fd);
-// 		printf("%i: %s\n", i, line);
-// 		free(line);
-// 		// printf("TEST RESULT: %s", get_next_line(fd));
-// 		i++;
-// 	}
-// 	return (1);
-// }
+	fd = open("test2.txt", O_RDONLY);
+	while ((line = get_next_line(fd)) != NULL && i < 20)
+	{
+		// printf("fd: %i", fd);
+		printf("%i: %s\n", i, line);
+		free(line);
+		// printf("TEST RESULT: %s", get_next_line(fd));
+		i++;
+	}
+	return (1);
+}
